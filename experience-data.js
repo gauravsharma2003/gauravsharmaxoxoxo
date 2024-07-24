34b5298a76e8f2583f2dd57ce40bfd5172bd41d0
Gaurav Shamra <sharmagauravxo@gmail.com>
2024-07-24 11:37:08 +0530

    exp added

---
+++ experience-data.js
@@ -0,0 +1,34 @@
+var experienceData = [
+    {
+      icon: "https://timesinternet.in/assets/images/logo-blue-new.png",
+      date: "Internship • July 2024 - Present",
+      organization: "Times Internet",
+      position: "Software Engineer Intern",
+      desc: `• Bug Fixing and Maintenance: Identified, diagnosed, and resolved several critical bugs, improving overall system stability and performance.
+      
+  • Cross-Team Collaboration: Worked closely with both backend and frontend teams to debug and optimize API endpoints, resulting in a 15% reduction in response time, and to address UI/UX issues, ensuring a seamless user experience across multiple devices.
+  
+  • Feature Implementation: Collaborated with the product manager to implement new features based on user feedback and market research, enhancing the product’s overall functionality and user satisfaction.
+  
+  • TOI App Development: Worked on developing and maintaining features for the Times of India app.`,
+      website: 'https://timesinternet.in'
+    },
+    {
+      icon: "https://iic.mic.gov.in/assets/assets/images/iiclogo.png",
+      date: "President • July 2023 - Present",
+      organization: "Institution's Innovation Council (IIC), GTBIT, GGSIPU",
+      position: "President",
+      desc: `Leading the council in fostering innovation and entrepreneurship within the institution. Organized events, workshops, and collaborations to promote a culture of innovation and creativity among students.
+      
+  • Hosted numerous events and workshops to inspire and educate students on various aspects of innovation and technology.
+  
+  • Collaborated with team members and external partners to bring valuable resources and opportunities to the council.
+  
+  • Implemented new initiatives to support student projects and startups, providing mentorship and guidance.
+  
+  • Encouraged cross-disciplinary collaboration and networking to enhance the innovation ecosystem within the institution.`,
+      website: 'https://iic.mic.gov.in/'
+    }
+];
+
+export default experienceData;
