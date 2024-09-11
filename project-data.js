aaaf567d112424cefaee606f6873ed5265dd1784
Gaurav Shamra <sharmagauravxo@gmail.com>
2024-09-11 20:31:15 +0530

    new project

--- project-data.js
+++ project-data.js
@@ -3,7 +3,6 @@ const projectData = [
     title: "Shoe-ping (E-Commerce)",
     miniDesc:
       "A web application that showcases a collection of shoes, allowing users to view, add, edit, and delete products. The application uses MongoDB, Express, Node.js, and EJS for rendering dynamic content.",
-
     github: "https://github.com/gauravsharma2003/ShoePing-Ecommerce",
     img: "/assets/images/projects/E-commerce.webp",
     seoimg: "/assets/images/projects/E-commerce.webp",
@@ -18,31 +17,32 @@ const projectData = [
       "Bootstrap",
       "GSAP"
     ],
-    desc: "A web application that showcases a collection of shoes, allowing users to view, add, edit, and delete products. The application uses MongoDB for data storage, Express and Node.js for the backend, and EJS for rendering dynamic content. Bootstrap and GSAP are used for responsive design and animations. This project demonstrates my full-stack development skills and ability to create interactive web applications.",
+    desc: "A web application that showcases a collection of shoes, allowing users to view, add, edit, and delete products. The application uses MongoDB for data storage, Express and Node.js for the backend, and EJS for rendering dynamic content. Bootstrap and GSAP are used for responsive design and animations. This project demonstrates my full-stack development skills and ability to create interactive web applications."
   },
   {
-    title: "Weather App",
+    title: "SehatSutra AI (Healthcare Solution)",
     miniDesc:
-      "A web application that provides real-time weather information for any location. It uses a weather API to fetch data and displays it in a user-friendly interface.",
-    website: "https://gauravsharma2003.github.io/My-Weather/",
-    github: "https://github.com/gauravsharma2003/My-Weather",
-    img: "/assets/images/projects/weather-app.webp",
-    seoimg: "/assets/images/seo/weather-app.webp",
-    link: "weather-app",
+      "AI-powered healthcare system with Aptos blockchain integration for secure payments and real-time diagnostics.",
+    github: "https://github.com/gauravsharma2003/SehatSutraAI",
+    img: "/assets/images/projects/sehatsutraAi.webp",
+    seoimg: "/assets/images/seo/sehatsutraAi.webp",
+    link: "sehat-sutra-ai",
     showOnHome: true,
     tech: [
-      "JavaScript",
-      "HTML",
-      "CSS",
-      "API",
+      "Aptos",
+      "Gemini AI",
+      "React.js",
+      "Tailwind CSS",
+      "Node.js",
+      "Express",
+      "MongoDB"
     ],
-    desc: "A web application that provides real-time weather information for any location. It uses a weather API to fetch data and displays it in a user-friendly interface. This project is developed using vanilla JavaScript, HTML, and CSS. It showcases my ability to work with APIs and create interactive web applications.",
+    desc: "A healthcare solution integrating AI for real-time diagnostics and Aptos blockchain for secure, transparent payments. Built with React.js, Tailwind CSS for frontend, Node.js with Express for the backend, and MongoDB for data storage. The system aims to reduce healthcare costs and improve access in underserved communities."
   },
   {
     title: "News App",
     miniDesc:
       "A news application that fetches and displays the latest news articles from various sources. The app uses a news API and presents the information in an organized manner.",
-    // website: "https://gauravsharma2003.github.io/My-news-app/",
     github: "https://github.com/gauravsharma2003/My-news-app",
     img: "/assets/images/projects/news-app.webp",
     seoimg: "/assets/images/seo/news-app.webp",
@@ -54,13 +54,30 @@ const projectData = [
       "CSS",
       "API",
     ],
-    desc: "A news application that fetches and displays the latest news articles from various sources. The app uses a news API and presents the information in an organized manner. This project highlights my ability to work with APIs and build responsive web applications.",
+    desc: "A news application that fetches and displays the latest news articles from various sources. The app uses a news API and presents the information in an organized manner. This project highlights my ability to work with APIs and build responsive web applications."
+  },
+  {
+    title: "Weather App",
+    miniDesc:
+      "A web application that provides real-time weather information for any location. It uses a weather API to fetch data and displays it in a user-friendly interface.",
+    website: "https://gauravsharma2003.github.io/My-Weather/",
+    github: "https://github.com/gauravsharma2003/My-Weather",
+    img: "/assets/images/projects/weather-app.webp",
+    seoimg: "/assets/images/seo/weather-app.webp",
+    link: "weather-app",
+    showOnHome: false,
+    tech: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "API",
+    ],
+    desc: "A web application that provides real-time weather information for any location. It uses a weather API to fetch data and displays it in a user-friendly interface. This project is developed using vanilla JavaScript, HTML, and CSS. It showcases my ability to work with APIs and create interactive web applications."
   },
   {
     title: "Snake Game",
     miniDesc:
       "A classic snake game developed using JavaScript, HTML, and CSS. The game features simple controls and an engaging gameplay experience.",
-    // website: "https://gauravsharma2003.github.io/Snake-game/",
     github: "https://github.com/gauravsharma2003/Snake-game",
     img: "/assets/images/projects/snake-game.webp",
     seoimg: "/assets/images/seo/snake-game.webp",
@@ -71,8 +88,9 @@ const projectData = [
       "HTML",
       "CSS",
     ],
-    desc: "A classic snake game developed using JavaScript, HTML, and CSS. The game features simple controls and an engaging gameplay experience. This project demonstrates my skills in game development and front-end technologies.",
-  }
+    desc: "A classic snake game developed using JavaScript, HTML, and CSS. The game features simple controls and an engaging gameplay experience. This project demonstrates my skills in game development and front-end technologies."
+  },
+  
 ];
 
 export default projectData;
